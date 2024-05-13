11c92491102b255536d438b21673eaef75981648
Anton <afeklichev@yandex.ru>
2024-05-13 10:01:43 +0300

    app relese

--- src/main/java/antonfeklichev/tasktrackerapp/mapper/SubTaskMapper.java
+++ src/main/java/antonfeklichev/tasktrackerapp/mapper/SubTaskMapper.java
@@ -5,22 +5,46 @@ import antonfeklichev.tasktrackerapp.dto.SubTaskDto;
 import antonfeklichev.tasktrackerapp.entity.SubTask;
 import org.mapstruct.*;
 
+/**
+ * Маппер для преобразования между сущностью {@link SubTask} и DTO {@link SubTaskDto}, {@link NewSubTaskDto}.
+ * <p>
+ * Аннотация {@link Mapper} указывает MapStruct, что этот интерфейс является маппером и должен быть реализован с поддержкой Spring.
+ * </p>
+ *
+ * @see SubTask
+ * @see SubTaskDto
+ * @see NewSubTaskDto
+ */
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface SubTaskMapper {
 
-    @Mapping(source = "name", target = "name")
-    @Mapping(source = "description", target = "description")
-    @Mapping(source = "status", target = "status")
+    /**
+     * Преобразует {@link NewSubTaskDto} в {@link SubTask}.
+     *
+     * @param dto объект {@link NewSubTaskDto}, содержащий данные для создания новой подзадачи.
+     * @return экземпляр {@link SubTask}, сформированный на основе данных из {@link NewSubTaskDto}.
+     */
     SubTask toSubTask(NewSubTaskDto dto);
 
-
-    @Mapping(source = "id", target = "id")
-    @Mapping(source = "name", target = "name")
-    @Mapping(source = "description", target = "description")
-    @Mapping(source = "status", target = "status")
+    /**
+     * Преобразует {@link SubTask} в {@link SubTaskDto}, включая маппинг идентификатора задачи.
+     *
+     * @param subTask объект {@link SubTask}, данные которого необходимо преобразовать в DTO.
+     * @return экземпляр {@link SubTaskDto}, содержащий данные из {@link SubTask}, включая идентификатор родительской задачи.
+     */
     @Mapping(source = "task.id", target = "taskId")
     SubTaskDto toSubTaskDto(SubTask subTask);
 
+    /**
+     * Обновляет существующий экземпляр {@link SubTask} на основе данных из {@link SubTaskDto}.
+     * <p>
+     * Метод аннотирован {@link BeanMapping} с стратегией {@link NullValuePropertyMappingStrategy IGNORE}, что означает,
+     * что нулевые значения в {@link SubTaskDto} не будут переноситься в {@link SubTask}, сохраняя текущие значения свойств {@link SubTask}.
+     * </p>
+     *
+     * @param subTask    объект {@link SubTask}, который требуется обновить.
+     * @param subTaskDto DTO, содержащее данные для обновления подзадачи.
+     */
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void patchSubTask(@MappingTarget SubTask subTask, SubTaskDto subTaskDto);
 
--- src/main/java/antonfeklichev/tasktrackerapp/mapper/TaskMapper.java
+++ src/main/java/antonfeklichev/tasktrackerapp/mapper/TaskMapper.java
@@ -5,20 +5,46 @@ import antonfeklichev.tasktrackerapp.dto.TaskDto;
 import antonfeklichev.tasktrackerapp.entity.Task;
 import org.mapstruct.*;
 
+/**
+ * Маппер для преобразования между сущностью {@link Task} и DTO {@link TaskDto}, {@link NewTaskDto}.
+ * <p>
+ * Аннотация {@link Mapper} указывает MapStruct, что этот интерфейс является маппером и должен быть реализован с поддержкой Spring.
+ * </p
+ *
+ * @see Task
+ * @see TaskDto
+ * @see NewTaskDto
+ */
+
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface TaskMapper {
 
-    @Mapping(source = "name", target = "name")
-    @Mapping(source = "description", target = "description")
-    @Mapping(source = "status", target = "status")
+    /**
+     * Преобразует {@link NewTaskDto} в {@link Task}.
+     *
+     * @param dto объект {@link NewTaskDto}, содержащий данные для создания новой задачи.
+     * @return экземпляр {@link Task}, сформированный на основе данных из {@link NewTaskDto}.
+     */
     Task toTask(NewTaskDto dto);
 
-    @Mapping(source = "id", target = "id")
-    @Mapping(source = "name", target = "name")
-    @Mapping(source = "description", target = "description")
-    @Mapping(source = "status", target = "status")
+    /**
+     * Преобразует {@link Task} в {@link TaskDto}.
+     *
+     * @param task объект {@link Task}, данные которого необходимо преобразовать в DTO.
+     * @return экземпляр {@link TaskDto}, содержащий данные из {@link Task}.
+     */
     TaskDto toTaskDto(Task task);
 
+    /**
+     * Обновляет существующий экземпляр {@link Task} на основе данных из {@link TaskDto}.
+     * <p>
+     * Метод аннотирован {@link BeanMapping} с стратегией {@link NullValuePropertyMappingStrategy IGNORE}, что означает,
+     * что нулевые значения в {@link TaskDto} не будут переноситься в {@link Task}, сохраняя текущие значения свойств {@link Task}.
+     * </p>
+     *
+     * @param task    объект {@link Task}, который требуется обновить.
+     * @param taskDto DTO, содержащее данные для обновления задачи.
+     */
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void patchTask(@MappingTarget Task task, TaskDto taskDto);
 }
